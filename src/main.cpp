a0d192d54050004ce6e50672dcba345d33fa26a2
youwenhei <youwenhei1110@gmail.com>
2026-07-08 21:42:19 +0800

    Implement inventory management in main.cpp
    Added inventory management functionality with sample product.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,35 @@
+#include "Inventory.h"
+#include "Product.h"
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
-  cout << "Welcome to YouExpress Inventory Management System!" << endl;
-  cout << "What service you want to use today?" << endl;
-}
+	SetConsoleOutputCP(CP_UTF8); // Set console output to UTF-8)
 
-return 0;
+	cout << "Welcome to YouExpress Inventory Management System!" << endl;
+
+	Inventory inventory; // Create an instance of Inventory
+	
+	//Sample product data
+	Product product1(
+		1,
+		123456789,
+		"I LOVE MEE",
+		"With flavoring powder and seasoning oil",
+		"Instant Noodles",
+		50,
+		4.90,
+		"My Company",
+		"2026-12-31",
+		"2025-01-01"
+	);
+
+	inventory.addProduct(product1);
+	inventory.displayProducts();
+
+	return 0;
+
+}
