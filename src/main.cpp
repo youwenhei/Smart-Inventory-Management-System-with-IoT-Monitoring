1d63464280f359569c0bb6b43788d2ce717c5dff
youwenhei <youwenhei1110@gmail.com>
2026-07-08 16:19:28 +0800

    Create main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+  cout << "Welcome to YouExpress Inventory Management System!" << endl;
+  cout << "What service you want to use today?" << endl;
+}
+
+return 0;
