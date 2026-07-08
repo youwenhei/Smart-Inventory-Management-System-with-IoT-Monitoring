9a790ec1deb7725c44833fe79bceecd915772283
youwenhei <youwenhei1110@gmail.com>
2026-07-08 16:30:01 +0800

    Adding function into Product.h

--- include/Product.h
+++ include/Product.h
@@ -1,4 +1,27 @@
-#ifndef PRODUCT_H
-#define PRODUCT_H
+//include guards
+//#ifndef PRODUCT_H
+//#define PRODUCT_H
+
+#pragma once
+#include <string>
+
+class Product
+{
+private: //cannot be edited
+  int proID;
+  string proName;
+  int proQuantity;
+  double proPrice;
+
+public: //product creation
+  Product(int proID, string proName, int proQuantity, double proPrice);
+
+  //system function
+  void display();
+
+  int getID();
+  string getName();
+  int getQuantity();
+  double getPrice();
 
 #endif
