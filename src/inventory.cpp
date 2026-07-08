c03cf721dc59a0a0da6f7cfab84892daa8b9e2d8
youwenhei <youwenhei1110@gmail.com>
2026-07-08 21:42:59 +0800

    Implement addProduct and displayProducts methods
    Added methods to add and display products in the inventory.

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -1,1 +1,14 @@
 #include "Inventory.h"
+#include <iostream>
+
+void Inventory::addProduct(const Product& product) {
+	products.push_back(product); //to add the product to the vector 
+	//(products(empty)) become (products(1)) after adding the product
+}
+
+void Inventory::displayProducts() const {
+	//take each product in the vector and display it
+	for (const auto& product : products) {
+		product.display();
+	}
+}
