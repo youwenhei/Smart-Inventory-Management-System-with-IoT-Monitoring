b27fbdf19988df654b43145db42a7881c6402b35
youwenhei <youwenhei1110@gmail.com>
2026-07-08 21:34:15 +0800

    Refactor Product.h to include guards and std::string
    Updated Product.h to use std::string for string attributes and added include guards.

--- include/Product.h
+++ include/Product.h
@@ -1,6 +1,6 @@
 //include guards
-//#ifndef PRODUCT_H
-//#define PRODUCT_H
+#ifndef PRODUCT_H
+#define PRODUCT_H
 
 #pragma once
 #include <string>
@@ -9,28 +9,29 @@ class Product
 {
 private: //cannot be edited
 	//product attributes
-  int product_ID;
-  int product_Barcode;
-  string product_Name;
-  string product_Description;
-  string product_Category;
+	int product_ID;
+	int product_Barcode;
+	std::string product_Name;
+	std::string product_Description;
+	std::string product_Category;
 
 	//product stock and price
-  int product_Quantity;
-  double product_Price;
+	int product_Quantity;
+	double product_Price;
 
 	//product supplier and dates
-  string product_Supplier;
-  string product_ExpiryDate;
-  string product_ManufactureDate;
+	std::string product_Supplier;
+	std::string product_ExpiryDate;
+	std::string product_ManufactureDate;
 
 
 public: //product creation
-	Product(int product_ID, int product_Barcode, string product_Name, string product_Description, string product_Category, 
-			int product_Quantity, double product_Price, 
-			string product_Supplier, string product_ExpiryDate, string product_ManufactureDate);
+	Product(int product_ID, int product_Barcode, std::string product_Name, std::string product_Description, std::string product_Category,
+		int product_Quantity, double product_Price,
+		std::string product_Supplier, std::string product_ExpiryDate, std::string product_ManufactureDate);
 
-  //system function
-  void display();
+	//system function
+	void display() const;
+};
 
 #endif
