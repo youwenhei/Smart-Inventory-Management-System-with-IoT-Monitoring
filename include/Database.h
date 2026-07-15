128f702a1a0f14c6f05d4d85891fdf490162f37b
YOU WEN HEI <youwenhei1110@gmail.com>
2026-07-16 01:08:58 +0800

    Introduce Product class in Database header
    Added Product class with attributes and methods for managing product details.

--- include/Database.h
+++ include/Database.h
@@ -1,19 +1,75 @@
-#ifndef DATABASE_H
-#define DATABASE_H
+#ifndef PRODUCT_H
+#define PRODUCT_H
 #pragma once
-#include <jdbc/mysql_driver.h>
-#include <jdbc/mysql_connection.h>
 
-class Database {
+#include <string>
+
+class Product
+{
 private:
-	sql::mysql::MySQL_Driver* driver;
-	sql::Connection* con;
+
+	//product attributes
+	int product_ID;
+	std::string product_Barcode;
+	std::string product_RFID_UID;
+	std::string product_Name;
+	std::string product_Description;
+	std::string product_Category;
+
+	//product stock and price
+	int product_Quantity;
+	double product_Price;
+
+	//product supplier and dates
+	std::string product_Supplier;
+	std::string product_ExpiryDate;
+	std::string product_ManufactureDate;
 
 public:
-	Database();
-	~Database();
 
-	sql::Connection* getConnection() const;
+	Product(int product_ID,
+		std::string product_Barcode,
+		std::string product_Name,
+		std::string product_Description,
+		std::string product_Category,
+		int product_Quantity,
+		double product_Price,
+		std::string product_Supplier,
+		std::string product_ExpiryDate,
+		std::string product_ManufactureDate);
+
+
+	//system function
+	void display() const;
+
+
+	//getter function
+	int getID() const;
+	std::string getBarcode() const;
+	std::string getName() const;
+	std::string getCategory() const;
+	std::string getExpiryDate() const;
+	std::string getManufactureDate() const;
+	double getPrice() const;
+	int getQuantity() const;
+	std::string getSupplier() const;
+	std::string getDescription() const;
+	double getInventoryValue() const;
+	std::string getRFID() const;
+
+
+	//setter function
+	void setBarcode(std::string newBarcode);
+	void setName(std::string newName);
+	void setDescription(std::string newDescription);
+	void setCategory(std::string newCategory);
+	void setQuantity(int newQuantity);
+	void setPrice(double newPrice);
+	void setSupplier(std::string newSupplier);
+	void setExpiryDate(std::string newExpiry);
+	void setManufactureDate(std::string newManufacture);
+	void setRFID(std::string newRFID);
+
 };
 
 #endif
